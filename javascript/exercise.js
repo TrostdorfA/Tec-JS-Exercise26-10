3c7639c5f715c44637bd94f6403680444eccb667
TrostdorfA <sebatros@hotmail.com>
2022-10-27 17:14:00 -0300

    :rocket: Fix: Updated exercise

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -250,12 +250,116 @@ for (let i = 0; i < userData.length; i++) {
   }
 }
 
-let userId = userData.id;
-switch ((userId = prompt("Ingrese el ID del usuario que desea buscar"))) {
-  case userId:
-    console.log(`El usuario con el ID ${userId} es ${userData.name}, su usuario es ${userData.username}, su correo es ${userData.email}, su telefono es ${userData.phone} y su pagina web es ${userData.website}
-        El nombre de su compañia es: ${userData.company.name} y su frase clave es: ${userData.company.catchPhrase}
-        Su direccion es: ${userData.address.street} ${userData.address.suite} ${userData.address.city} ${userData.address.zipcode}`);
+// Un modo
+switch (prompt("Ingrese el ID del usuario que desea ver: ")) {
+  case "1":
+    console.log(`El usuario con el ID ${userData[0].id} es: ${userData[0].name}, su usuario es: ${userData[0].username}, su correo es: ${userData[0].email}, su telefono es: ${userData[0].phone} y su pagina web es: ${userData[0].website}
+      Su compañia es: ${userData[0].company.name}, su frase clave es: ${userData[0].company.catchPhrase} y su slogan es: ${userData[0].company.bs}
+      Su direccion es: ${userData[0].address.street}, ${userData[0].address.suite}, ${userData[0].address.city}, ${userData[0].address.zipcode}`);
+    break;
+  case "2":
+    console.log(`El usuario con el ID ${userData[1].id} es: ${userData[1].name}, su usuario es: ${userData[1].username}, su correo es: ${userData[1].email}, su telefono es: ${userData[1].phone} y su pagina web es: ${userData[1].website}
+      Su compañia es: ${userData[1].company.name}, su frase clave es: ${userData[1].company.catchPhrase} y su slogan es: ${userData[1].company.bs}
+      Su direccion es: ${userData[1].address.street}, ${userData[1].address.suite}, ${userData[1].address.city}, ${userData[1].address.zipcode}`);
+    break;
+  case "3":
+    console.log(`El usuario con el ID ${userData[2].id} es: ${userData[2].name}, su usuario es: ${userData[2].username}, su correo es: ${userData[2].email}, su telefono es: ${userData[2].phone} y su pagina web es: ${userData[2].website}
+      Su compañia es: ${userData[2].company.name}, su frase clave es: ${userData[2].company.catchPhrase} y su slogan es: ${userData[2].company.bs}
+      Su direccion es: ${userData[2].address.street}, ${userData[2].address.suite}, ${userData[2].address.city}, ${userData[2].address.zipcode}`);
+    break;
+  case "4":
+    console.log(`El usuario con el ID ${userData[3].id} es: ${userData[3].name}, su usuario es: ${userData[3].username}, su correo es: ${userData[3].email}, su telefono es: ${userData[3].phone} y su pagina web es: ${userData[3].website}
+      Su compañia es: ${userData[3].company.name}, su frase clave es: ${userData[3].company.catchPhrase} y su slogan es: ${userData[3].company.bs}
+      Su direccion es: ${userData[3].address.street}, ${userData[3].address.suite}, ${userData[3].address.city}, ${userData[3].address.zipcode}`);
+    break;
+  case "5":
+    console.log(`El usuario con el ID ${userData[4].id} es: ${userData[4].name}, su usuario es: ${userData[4].username}, su correo es: ${userData[4].email}, su telefono es: ${userData[4].phone} y su pagina web es: ${userData[4].website}
+      Su compañia es: ${userData[4].company.name}, su frase clave es: ${userData[4].company.catchPhrase} y su slogan es: ${userData[4].company.bs}
+      Su direccion es: ${userData[4].address.street}, ${userData[4].address.suite}, ${userData[4].address.city}, ${userData[4].address.zipcode}`);
+    break;
+  case "6":
+    console.log(`El usuario con el ID ${userData[5].id} es: ${userData[5].name}, su usuario es: ${userData[5].username}, su correo es: ${userData[5].email}, su telefono es: ${userData[5].phone} y su pagina web es: ${userData[5].website}
+      Su compañia es: ${userData[5].company.name}, su frase clave es: ${userData[5].company.catchPhrase} y su slogan es: ${userData[5].company.bs}
+      Su direccion es: ${userData[5].address.street}, ${userData[5].address.suite}, ${userData[5].address.city}, ${userData[5].address.zipcode}`);
+    break;
+  case "7":
+    console.log(`El usuario con el ID ${userData[6].id} es: ${userData[6].name}, su usuario es: ${userData[6].username}, su correo es: ${userData[6].email}, su telefono es: ${userData[6].phone} y su pagina web es: ${userData[6].website}
+      Su compañia es: ${userData[6].company.name}, su frase clave es: ${userData[6].company.catchPhrase} y su slogan es: ${userData[6].company.bs}
+      Su direccion es: ${userData[6].address.street}, ${userData[6].address.suite}, ${userData[6].address.city}, ${userData[6].address.zipcode}`);
+    break;
+  case "8":
+    console.log(`El usuario con el ID ${userData[7].id} es: ${userData[7].name}, su usuario es: ${userData[7].username}, su correo es: ${userData[7].email}, su telefono es: ${userData[7].phone} y su pagina web es: ${userData[7].website}
+      Su compañia es: ${userData[7].company.name}, su frase clave es: ${userData[7].company.catchPhrase} y su slogan es: ${userData[7].company.bs}
+      Su direccion es: ${userData[7].address.street}, ${userData[7].address.suite}, ${userData[7].address.city}, ${userData[7].address.zipcode}`);
+    break;
+  case "9":
+    console.log(`El usuario con el ID ${userData[8].id} es: ${userData[8].name}, su usuario es: ${userData[8].username}, su correo es: ${userData[8].email}, su telefono es: ${userData[8].phone} y su pagina web es: ${userData[8].website}
+      Su compañia es: ${userData[8].company.name}, su frase clave es: ${userData[8].company.catchPhrase} y su slogan es: ${userData[8].company.bs}
+      Su direccion es: ${userData[8].address.street}, ${userData[8].address.suite}, ${userData[8].address.city}, ${userData[8].address.zipcode}`);
+    break;
+  case "10":
+    console.log(`El usuario con el ID ${userData[9].id} es: ${userData[9].name}, su usuario es: ${userData[9].username}, su correo es: ${userData[9].email}, su telefono es: ${userData[9].phone} y su pagina web es: ${userData[9].website}
+      Su compañia es: ${userData[9].company.name}, su frase clave es: ${userData[9].company.catchPhrase} y su slogan es: ${userData[9].company.bs}
+      Su direccion es: ${userData[9].address.street}, ${userData[9].address.suite}, ${userData[9].address.city}, ${userData[9].address.zipcode}`);
+    break;
+  default:
+    console.log("No existe el usuario");
+    break;
+}
+
+// Otro modo de hacerlo
+let userId = prompt("Ingrese el ID del usuario que desea buscar entre 1 y 10");
+userId = Number(userId);
+switch (userId) {
+  case (userId = userData[0].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[0].name}, su usuario es: ${userData[0].username}, su correo es: ${userData[0].email}, su telefono es: ${userData[0].phone} y su pagina web es: ${userData[0].website}
+    Su compañia es: ${userData[0].company.name}, su frase clave es: ${userData[0].company.catchPhrase} y su slogan es: ${userData[0].company.bs}
+    Su direccion es: ${userData[0].address.street}, ${userData[0].address.suite}, ${userData[0].address.city}, ${userData[0].address.zipcode}`);
+    break;
+  case (userId = userData[1].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[1].name}, su usuario es: ${userData[1].username}, su correo es: ${userData[1].email}, su telefono es: ${userData[1].phone} y su pagina web es: ${userData[1].website}
+    Su compañia es: ${userData[1].company.name}, su frase clave es: ${userData[1].company.catchPhrase} y su slogan es: ${userData[1].company.bs}
+    Su direccion es: ${userData[1].address.street}, ${userData[1].address.suite}, ${userData[1].address.city}, ${userData[1].address.zipcode}`);
+    break;
+  case (userId = userData[2].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[2].name}, su usuario es: ${userData[2].username}, su correo es: ${userData[2].email}, su telefono es: ${userData[2].phone} y su pagina web es: ${userData[2].website}
+    Su compañia es: ${userData[2].company.name}, su frase clave es: ${userData[2].company.catchPhrase} y su slogan es: ${userData[2].company.bs}
+    Su direccion es: ${userData[2].address.street}, ${userData[2].address.suite}, ${userData[2].address.city}, ${userData[2].address.zipcode}`);
+    break;
+  case (userId = userData[3].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[3].name}, su usuario es: ${userData[3].username}, su correo es: ${userData[3].email}, su telefono es: ${userData[3].phone} y su pagina web es: ${userData[3].website}
+    Su compañia es: ${userData[3].company.name}, su frase clave es: ${userData[3].company.catchPhrase} y su slogan es: ${userData[3].company.bs}
+    Su direccion es: ${userData[3].address.street}, ${userData[3].address.suite}, ${userData[3].address.city}, ${userData[3].address.zipcode}`);
+    break;
+  case (userId = userData[4].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[4].name}, su usuario es: ${userData[4].username}, su correo es: ${userData[4].email}, su telefono es: ${userData[4].phone} y su pagina web es: ${userData[4].website}
+    Su compañia es: ${userData[4].company.name}, su frase clave es: ${userData[4].company.catchPhrase} y su slogan es: ${userData[4].company.bs}
+    Su direccion es: ${userData[4].address.street}, ${userData[4].address.suite}, ${userData[4].address.city}, ${userData[4].address.zipcode}`);
+    break;
+  case (userId = userData[5].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[5].name}, su usuario es: ${userData[5].username}, su correo es: ${userData[5].email}, su telefono es: ${userData[5].phone} y su pagina web es: ${userData[5].website}
+    Su compañia es: ${userData[5].company.name}, su frase clave es: ${userData[5].company.catchPhrase} y su slogan es: ${userData[5].company.bs}
+    Su direccion es: ${userData[5].address.street}, ${userData[5].address.suite}, ${userData[5].address.city}, ${userData[5].address.zipcode}`);
+    break;
+  case (userId = userData[6].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[6].name}, su usuario es: ${userData[6].username}, su correo es: ${userData[6].email}, su telefono es: ${userData[6].phone} y su pagina web es: ${userData[6].website}
+    Su compañia es: ${userData[6].company.name}, su frase clave es: ${userData[6].company.catchPhrase} y su slogan es: ${userData[6].company.bs}
+    Su direccion es: ${userData[6].address.street}, ${userData[6].address.suite}, ${userData[6].address.city}, ${userData[6].address.zipcode}`);
+    break;
+  case (userId = userData[7].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[7].name}, su usuario es: ${userData[7].username}, su correo es: ${userData[7].email}, su telefono es: ${userData[7].phone} y su pagina web es: ${userData[7].website}
+    Su compañia es: ${userData[7].company.name}, su frase clave es: ${userData[7].company.catchPhrase} y su slogan es: ${userData[7].company.bs}
+    Su direccion es: ${userData[7].address.street}, ${userData[7].address.suite}, ${userData[7].address.city}, ${userData[7].address.zipcode}`);
+    break;
+  case (userId = userData[8].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[8].name}, su usuario es: ${userData[8].username}, su correo es: ${userData[8].email}, su telefono es: ${userData[8].phone} y su pagina web es: ${userData[8].website}
+    Su compañia es: ${userData[8].company.name}, su frase clave es: ${userData[8].company.catchPhrase} y su slogan es: ${userData[8].company.bs}
+    Su direccion es: ${userData[8].address.street}, ${userData[8].address.suite}, ${userData[8].address.city}, ${userData[8].address.zipcode}`);
+    break;
+  case (userId = userData[9].id):
+    console.log(`El usuario con el ID ${userId} es: ${userData[9].name}, su usuario es: ${userData[9].username}, su correo es: ${userData[9].email}, su telefono es: ${userData[9].phone} y su pagina web es: ${userData[9].website}
+    Su compañia es: ${userData[9].company.name}, su frase clave es: ${userData[9].company.catchPhrase} y su slogan es: ${userData[9].company.bs}
+    Su direccion es: ${userData[9].address.street}, ${userData[9].address.suite}, ${userData[9].address.city}, ${userData[9].address.zipcode}`);
     break;
   default:
     console.log("El usuario no existe");
